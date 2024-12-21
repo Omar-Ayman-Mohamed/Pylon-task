01afc3a02bf061868a2977578921f7cc2993f55f
omar ayman <61347450+warior65048@users.noreply.github.com>
2024-12-21 23:39:27 +0200

    added documntation

--- Core/Inc/key_stoarge.h
+++ Core/Inc/key_stoarge.h
@@ -4,16 +4,21 @@
  *  Created on: Dec 21, 2024
  *      Author: wario
  */
-
+/*this file includes global keys for encryption and decryption
+ * must be protected by securing it , using password to acess , save in a secure place etc.
+ * */
 #ifndef INC_KEY_STOARGE_H_
 #define INC_KEY_STOARGE_H_
 #include"AES_User_defines.h"
+/*key and iv for AES-CBC operations */
 const unsigned char key[32] = "0123456789abcdef0123456789abcdef"; // 256-bit key
 const unsigned char iv[AES_BLOCK_SIZE] = "0123456789abcdef";        // 128-bit IV
+/*key,iv and aad for AES-GCM operations */
 const unsigned char aad[] = "header data";
 const unsigned char GCM_key[AES_GCM_KEY_SIZE] = "abcdefghilmnopq";
 const unsigned char GCM_iv[AES_GCM_IV_SIZE] = "123456789101";
 size_t aad_len = strlen((char *)aad);
+/*personalized string for random data generation*/
 const unsigned char pers[] = "ahmed is -";
 size_t pers_len = strlen((char *)pers);
 #endif /* INC_KEY_STOARGE_H_ */
--- Core/Src/encryption.c
+++ Core/Src/encryption.c
@@ -25,7 +25,10 @@ void padding_data_for_AES_CBC_encryption(unsigned char*original_text,size_t orig
 /*
  * function that encrypts the data using AES-CBC encryption ,
  * it takes data in blocks of 16 bytes , so check if padding is needed
- * Parms : padded_message_len length of message to be encrypted after padding
+ * Parms :
+ * key:provide key for encryption can be saved in secure place or randomly generated
+ * iv: intilization vector for encryption can be saved in secure place or randomly generated
+ *  padded_message_len length of message to be encrypted after padding
  * padded_clear_message : padded message to be encrypted
  * padded_encrypted_message : output buffer for data after encryption
  *
@@ -42,12 +45,21 @@ if (mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, padded_message_len, iv, pad
     printf("Encryption failed\n");
     return 1;
 }
+/*always make sure to free resources after finishing*/
 mbedtls_aes_free(&aes);
 }
+/*
+ * function that decrypts the data using AES-CBC encryption ,
+ * it takes data in blocks of 16 bytes , so check if padding is needed
+ * Parms :
+ * key:provide key for encryption can be saved in secure place or randomly generated
+ * iv: intilization vector for encryption can be saved in secure place or randomly generated
+ *  padded_message_len length of message to be encrypted after padding
+ * padded_clear_message : padded message to be encrypted
+ * padded_encrypted_message : output buffer for data after encryption
+ *
+ *  */
 void AES_Decryption(unsigned char *key,unsigned char *iv,size_t padded_message_len ,unsigned char * padded_Encrypted_message,unsigned char*padded_decrytped_message){
-
-// Decryption
-//   memcpy(iv_copy, iv, AES_BLOCK_SIZE); // Reset IV for decryption
     mbedtls_aes_context aes;
     mbedtls_aes_init(&aes);
 if (mbedtls_aes_setkey_dec(&aes, key, 256) != 0) {
@@ -59,8 +71,23 @@ if (mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, padded_message_len, iv, pad
     printf("Decryption failed\n");
     return 1;
 }
+/*always make sure to free resources after finishing*/
 mbedtls_aes_free(&aes);
 }
+
+/*
+ * function that encrypts the data using AES-GCM encryption ,
+ * it takes addtional parameters as tag and aad for data integrity checks
+ * Parms :
+ * key:provide key for encryption can be saved in secure place or randomly generated
+ * KEY_SIZE : size of supplied key
+ * iv: intilization vector for encryption can be saved in secure place or randomly generated
+ * IV_SIZE : size of supplied iv
+ * input : data to be encrypted
+ * output : output buffer
+ * aad : addtive authentication data
+ * tag : for data integurity check
+ *  */
 
 int aes_gcm_encrypt(unsigned char *key,size_t KEY_SIZE,unsigned char *iv,size_t IV_SIZE,unsigned char * input, size_t input_len,
 		 unsigned char * output,unsigned char *aad,size_t aad_len, unsigned char * tag)
@@ -70,9 +97,23 @@ int aes_gcm_encrypt(unsigned char *key,size_t KEY_SIZE,unsigned char *iv,size_t 
     mbedtls_gcm_setkey(&gcm, MBEDTLS_CIPHER_ID_AES, key, KEY_SIZE * 8);
     int ret = mbedtls_gcm_crypt_and_tag(&gcm, MBEDTLS_GCM_ENCRYPT, input_len, iv, IV_SIZE,
                               aad, aad_len, input, output, TAG_SIZE, tag);
+    /*always make sure to free resources after finishing*/
     mbedtls_gcm_free(&gcm);
     return ret;
 }
+/*
+ * function that decrypts the data using AES-GCM encryption ,
+ * it takes addtional parameters as tag and aad for data integrity checks
+ * Parms :
+ * key:provide key for encryption can be saved in secure place or randomly generated
+ * KEY_SIZE : size of supplied key
+ * iv: intilization vector for encryption can be saved in secure place or randomly generated
+ * IV_SIZE : size of supplied iv
+ * input : data to be encrypted
+ * output : output buffer
+ * aad : addtive authentication data
+ * tag : for data integurity check
+ *  */
 int aes_gcm_decrypt(unsigned char *key,size_t KEY_SIZE,unsigned char *iv,size_t IV_SIZE,unsigned char *input, size_t input_len,
 		unsigned char *output,unsigned char *aad,size_t aad_len ,unsigned char *tag)
 {
@@ -84,17 +125,29 @@ int aes_gcm_decrypt(unsigned char *key,size_t KEY_SIZE,unsigned char *iv,size_t 
     mbedtls_gcm_free(&gcm);
     return ret;
 }
-
+/**
+ * function that generates random data for generation of random keys and ivs for addtional securtiy
+ * best used for tempory communication that is needed for only one session as the key and iv are different
+ * each time the program runs
+ * parms :
+ * buffer : input container for random data to be generated
+ * personlaized text : any string to ensure randomnnes of generated data
+ *  personalized_text_len : length of personalized string
+ *
+ *
+ * */
 
 void generate_random(unsigned char *buffer, size_t length,unsigned char*personalized_text,size_t personalized_text_len) {
     mbedtls_entropy_context entropy;
     mbedtls_ctr_drbg_context ctr_drbg;
-
+    /*intilization*/
     mbedtls_entropy_init(&entropy);
     mbedtls_ctr_drbg_init(&ctr_drbg);
+    /*seed generation*/
     mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, personalized_text, personalized_text_len);
+   /*random data generation*/
     mbedtls_ctr_drbg_random(&ctr_drbg, buffer, length);
-
+    /*freeing resources after finishing*/
     mbedtls_ctr_drbg_free(&ctr_drbg);
     mbedtls_entropy_free(&entropy);
 }
